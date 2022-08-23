a2d6749de3bb817c4e5d6fb534f6fc5fd45e918f
Leonardo Naoto Hirosue <leonardo.hirosue@gmail.com>
2022-08-23 12:49:18 -0300

    Exportando e importando módulos nativos do ES6+

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-const Planet = require('./planet')
-const solarSystem = require('./solar_system')
-const moment = require('moment')
+import Spaceship from "./spaceship"
+import armamentKind from "./armaments"
 
+const spaceship = new Spaceship("USS Enterprise", "James Tiberiu Kirt", armamentKind.laser)
 
-const earth = new Planet('Terra', 50100000)
-earth.rotate()
-
-solarSystem.planets.push(earth)
-solarSystem.planets.push(new Planet('Marte', 144800000))
-solarSystem.planets.push(new Planet('Mercúrio', 74800000))
-solarSystem.planets.push(new Planet('Saturno',4270000000))
-
-console.log(solarSystem)
-console.log(moment().format('DD-MM-YYYY'))
+console.log(spaceship)
