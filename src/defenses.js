8293237e8bb84ce7c546dd6e46a21ba90b393e9e
Leonardo Naoto Hirosue <leonardo.hirosue@gmail.com>
2022-08-23 14:25:17 -0300

    Inserção de módulos nomeados

---
+++ src/defenses.js
@@ -0,0 +1,5 @@
+const laserDefenses = ["Escudos Defletrores", "Arma anti-laser"]
+const steelDefenses = ["Escudos de Aço"]
+
+export { laserDefenses, steelDefenses}
+export default ["Escudo Repulsor"]
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import Spaceship from "./spaceship"
 import armamentKind from "./armaments"
+//import nomeado
+import { laserDefenses } from "./defenses.js"
+import basicDefense from "./defenses.js"
 
-const spaceship = new Spaceship("USS Enterprise", "James Tiberiu Kirt", armamentKind.laser)
+const spaceship = new Spaceship("USS Enterprise", "James Tiberiu Kirt", armamentKind.laser, laserDefenses)
+const basicSpaceship = new Spaceship("Nave Básica", "Astronauta Zin", [], basicDefense)
 
 console.log(spaceship)
+console.log(basicSpaceship)
