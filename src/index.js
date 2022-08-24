5437cc874f4d7992e17c3dfe15c35f9d39877a1f
Leonardo Naoto Hirosue <leonardo.hirosue@gmail.com>
2022-08-24 11:33:48 -0300

    Utilizando o Cath

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let komodoShip = {
 	nome: "Komodo",
 	velocity: 80,
-	acceleration: 10
+	acceleration: 0
 }
 
 const velocityAfter2seconds = (velocity, acceleration) => {
@@ -22,6 +22,8 @@ const velocityAfter2seconds = (velocity, acceleration) => {
 velocityAfter2seconds(komodoShip.velocity, komodoShip.acceleration).then(velocity => {
 	komodoShip.velocity = velocity
 	console.log("Após aceleração: ", komodoShip)
+}).catch(message => {
+	console.log(`Komodo: ${message}`)
 })
 
 console.log("Execução de Promises")
