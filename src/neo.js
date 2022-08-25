c43307b211a045a19bf3e83dbad6d1c0afa0b791
Leonardo Naoto Hirosue <leonardo.hirosue@gmail.com>
2022-08-25 16:16:25 -0300

    Consumindo a API da NASA

---
+++ src/neo.js
@@ -0,0 +1,8 @@
+export default class Neo {
+    constructor(id, name, avarageEstimatedDiameter, isSentry){
+        this.id = id,
+        this.name = name,
+        this.avarageEstimatedDiameter = avarageEstimatedDiameter,
+        this.isSentry = isSentry
+    }
+}
