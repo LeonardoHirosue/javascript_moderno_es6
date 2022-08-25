f808e610b8487dfcfc9e2a91f0274752e888b305
Leonardo Naoto Hirosue <leonardo.hirosue@gmail.com>
2022-08-25 17:23:55 -0300

    Iniciando fetch

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { getSentry } from "./sentry_service";
+import Sentry from "./sentry";
+
+function 
---
+++ src/sentry.js
@@ -0,0 +1,8 @@
+export default class Sentry{
+    constructor(id, name, yearsMin, yearsMax){
+        this.id = id,
+        this.name = name,
+        this.yearsMin = yearsMin,
+        this.yearsMax = yearsMax
+    }
+}
